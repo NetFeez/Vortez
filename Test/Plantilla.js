aa7bee52ca60134fd3f96b3b1ce5e2fc51337e42
NetFeez <netfeez.dev@gmail.com>
2023-11-10 14:42:34 -0500

    Se optimiso el sistema de plantillas para objetos

--- Test/Plantilla.js
+++ Test/Plantilla.js
@@ -4,9 +4,6 @@ import Plantilla from "../Template/Template.js";
     await Plantilla.Load('./Test/Plantilla.vhtml', {
         Titulo: 'Titulo de la prueba',
         Des: 'Descripción xD',
-        AR: {
-            Algo: 'Algo 1',
-            Otra: 'Otra2'
-        }
+        Tests: ["algo", "algo2"]
     })
 ))();
