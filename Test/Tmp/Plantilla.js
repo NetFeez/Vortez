8fdbde35bc47e2915328eddc6abf628976823395
NetFeez <netfeez.dev@gmail.com>
2024-07-27 05:02:25 +0000

    actualizacion a ts

--- Test/Tmp/Plantilla.js
+++
@@ -1,9 +0,0 @@
-import Plantilla from "../../Template/Template.js";
-
-(async () => console.log(
-    await Plantilla.Load('./Test/Plantilla.vhtml', {
-        Titulo: 'Titulo de la prueba',
-        Des: 'Descripción xD',
-        Tests: ["algo", "algo2"]
-    })
-))();
