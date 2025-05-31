e39e8f5e44469435b17415a3a0161254d5ebf8c9
NetFeez <netfeez.dev@gmail.com>
2025-05-31 08:41:26 +0000

    feat: export Logger from Vortez.ts

--- src/Vortez.ts
+++ src/Vortez.ts
@@ -1,30 +1,30 @@
 /**
  * @author NetFeez <netfeez.dev@gmail.com>
- * @description Exporta lo necesario para usar Vortez.
+ * @description Exports the main utilities of the Vortez module.
  * @module NetFeez-Labs.Vortez
  * @license Apache-2.0
  */
 
-// Utilidades principales del modulo.
+// Imports the main utilities of the Vortez module.
 import Config from "./Config.js";
 import Debug from "./Debug.js";
+import Logger from "./LoggerManager/Logger.js";
 import Template from "./Template.js";
-import Vortez from "./Server/Server.js";
+import Server from "./Server/Server.js";
 import Utilities from "./Utilities/Utilities.js";
 
-// Utilidades en desarrollo y sin añadir (Beta).
+// Imports the beta utilities of the Vortez module.
 import JwtManager from "./Beta/JwtManager.js";
 import Mail from "./Beta/Mail.js";
 
-// Para exportar las utilidades en desarrollo y sin añadir.
-const Beta = {
-	JwtManager, Mail
-};
+// Exports the beta utilities of the Vortez module.
+export const Beta = { JwtManager, Mail };
 
-// Exportación de utilidades.
+// Exports the main utilities of the Vortez module.
+export const Vortez = Server;
 export {
-	Config, Debug, Template, Vortez, Utilities, Beta
+	Debug, Logger,
+	Utilities, Template,
+	Config,
 };
-
-// Exportación por defecto de Vortez.
 export default Vortez;
