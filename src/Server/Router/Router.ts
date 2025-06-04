8e3ceaf602b957b954414e909a99afeb9fe66b9b
NetFeez <netfeez.dev@gmail.com>
2025-06-04 14:07:04 +0000

    docs: added file headers

--- src/Server/Router/Router.ts
+++ src/Server/Router/Router.ts
@@ -1,3 +1,9 @@
+/**
+ * @author NetFeez <netfeez.dev@gmail.com>
+ * @description Manages the routing of the requests and WebSocket connections.
+ * @license Apache-2.0
+*/
+
 import HTTP from 'http';
 import { Duplex } from 'stream';
 
