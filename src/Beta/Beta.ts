11e7d28bb38df8632211d30fe476ca072285177e
NetFeez <netfeez.dev@gmail.com>
2025-08-30 16:28:39 +0000

    chore(export): new structure for export import

---
+++ src/Beta/Beta.ts
@@ -0,0 +1,9 @@
+/**
+ * @author NetFeez <netfeez.dev@gmail.com>
+ * @description Provides the beta functions and utilities.
+ * @module NetFeez-Labs.Vortez
+ * @license Apache-2.0
+ */
+
+export { JwtManager } from './JwtManager.js';
+export { Mail } from './Mail.js';
