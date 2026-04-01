12765ba221964e1b32d3adfd7e6403d30ee53406
NetFeez <netfeez.dev@gmail.com>
2026-04-01 17:57:07 -0500

    feat: added --dev flag for pubish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,7 +2,12 @@
 echo "Starting publish process...";
 bash scripts/compile.sh;
 
-echo "Publishing...";
-npm publish;
+if [[ "$1" == "--dev" ]]; then
+  echo "Publishing with tag 'dev'...";
+  npm publish --tag dev;
+else
+  echo "Publishing...";
+  npm publish;
+fi
 
 echo "Publish completed successfully!";
