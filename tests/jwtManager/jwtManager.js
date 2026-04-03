30f9fcb773c92254345e1c685577c3f5a7c3d26e
NetFeez <netfeez.dev@gmail.com>
2026-04-03 21:55:38 +0000

    test(jwt): update tests to align with new JwtManager implementation

--- tests/jwtManager/jwtManager.js
+++ tests/jwtManager/jwtManager.js
@@ -1,108 +1,76 @@
-import { JwtManager } from "../../build/Beta/JwtManager/JwtManager.js";
-import CRYPTO from 'crypto';
-import Utilities from "../../build/Utilities.js";
+// @ts-check
+import JwtManager from "../../build/beta/JwtManager/JwtManager.js";
 import { strict as assert } from 'assert';
 
+/** @type { JwtManager.Algorithm.HashLength[] } */
 const MASKS = ['256', '384', '512'];
-const SECRET = 'secret';
 
-const { publicKey: RSA_PUB_KEY, privateKey: RSA_PRIV_KEY } = CRYPTO.generateKeyPairSync('rsa', {
-    modulusLength: 2048,
-    publicKeyEncoding: {
-        type: 'spki',
-        format: 'pem'
-    },
-    privateKeyEncoding: {
-        type: 'pkcs8',
-        format: 'pem'
-    }
-});
+const { pub: HMAC_PUB_KEY, key: SECRET }      = JwtManager.KeyGenerator.generate('secret');
+const { pub: RSA_PUB_KEY, key: RSA_PRIV_KEY } = JwtManager.KeyGenerator.generate('rsa');
+const { pub: PSS_PUB_KEY, key: PSS_PRIV_KEY } = JwtManager.KeyGenerator.generate('rsa-pss');
+const { pub: EC_PUB_KEY,  key: EC_PRIV_KEY }  = JwtManager.KeyGenerator.generate('ec');
 
-const { publicKey: PSS_PUB_KEY, privateKey: PSS_PRIV_KEY } = CRYPTO.generateKeyPairSync('rsa-pss', {
-    modulusLength: 2048,
-    publicKeyEncoding: {
-        type: 'spki',
-        format: 'pem'
-    },
-    privateKeyEncoding: {
-        type: 'pkcs8',
-        format: 'pem'
-    }
-});
-
-const { publicKey: EC_PUB_KEY, privateKey: EC_PRIV_KEY } = CRYPTO.generateKeyPairSync('ec', {
-    namedCurve: 'secp256k1',
-    publicKeyEncoding: {
-        type: 'spki',
-        format: 'pem'
-    },
-    privateKeyEncoding: {
-        type: 'pkcs8',
-        format: 'pem'
-    }
-});
-
-const body = {
-    test: 'test',
-    XD: "xd"
+const payload = {
+    uuid: '00000000-0000-0000-0000-000000000000',
+    name: 'NetFeez',
+    email: 'netfeez.dev@gmail.com'
 };
 
-const options = {
-    expire: new Date(Date.now() + 10000)
+const header = {
+    exp: Math.floor((Date.now() + 10000) / 1000)
 };
-
-const testBase64UrlEncode = () => {
-    assert.equal(Utilities.base64UrlEncode('hello world'), 'aGVsbG8gd29ybGQ');
-    console.log('Utilities.base64UrlEncode passed');
-};
-
-const testBase64UrlDecode = () => {
-    assert.equal(Utilities.base64UrlDecode('aGVsbG8gd29ybGQ'), 'hello world');
-    console.log('Utilities.base64UrlDecode passed');
-};
-
-const testJwtManagerSignVerify = (algorithm, key, pubKey) => {
-    const jwt = new JwtManager(algorithm, key);
+/**
+ * Tests the signing and verification of JWTs using the specified algorithm and key.
+ * @param { JwtManager.AlgorithmName } algorithm The name of the algorithm to be tested (e.g., 'HS256', 'RS256', etc.).
+ * @param {  string } key The secret key or private key to be used for signing and verifying JWTs.
+ * @param {  string | null } pubKey The public key to be used for verifying JWTs (required for asymmetric algorithms).
+ */
+const testJwtManagerSignVerify = (algorithm, key, pubKey = null) => {
+    const jwt = new JwtManager(algorithm, {
+        key: key,
+        pub: pubKey || null
+    });
     console.log('--------------------------')
-    const JWTObject = jwt.sign(body, options);
-    console.log('jwt:', JWTObject);
-    if (algorithm.startsWith('HS')) {
-        console.log('secret:', key);
-    } else {
+    const token = jwt.sign(payload, header);
+    console.log('jwt:', token);
+    /* if (algorithm.startsWith('HS')) console.log('secret:', key);
+    else {
         console.log('Private Key:', key);
-        if (pubKey) {
-            console.log('Public Key:', pubKey);
-        }
+        if (pubKey) console.log('Public Key:', pubKey);
+    } */
+    try {
+        const parsed = jwt.parse(token);
+        console.log(parsed, '\n');
+        assert.deepEqual(parsed.payload, payload);
+        console.log(`${algorithm} test passed`);
+    } catch (error) {
+        console.error(`${algorithm} test failed:`, error);
     }
-    const verificationResult = jwt.verify(JWTObject);
-    console.log(verificationResult, '\n');
-    assert.equal(verificationResult.verify, true);
-    assert.deepEqual(verificationResult.body, body);
-    console.log(`${algorithm} test passed`);
 };
 
 const runTests = () => {
     try {
-        testBase64UrlEncode();
-        testBase64UrlDecode();
-
         for (let index = 0; index < MASKS.length; index++) {
-            const algorithm = 'HS' + MASKS[index];
+            /** @type { JwtManager.AlgorithmName } */
+            const algorithm = `HS${MASKS[index]}`;
             testJwtManagerSignVerify(algorithm, SECRET);
 
-            const algorithmRS = 'RS' + MASKS[index];
+            /** @type { JwtManager.AlgorithmName } */
+            const algorithmRS = `RS${MASKS[index]}`;
             testJwtManagerSignVerify(algorithmRS, RSA_PRIV_KEY, RSA_PUB_KEY);
 
-            const algorithmPS = 'PS' + MASKS[index];
+            /** @type { JwtManager.AlgorithmName } */
+            const algorithmPS = `PS${MASKS[index]}`;
             testJwtManagerSignVerify(algorithmPS, PSS_PRIV_KEY, PSS_PUB_KEY);
 
-            const algorithmES = 'ES' + MASKS[index];
+            /** @type { JwtManager.AlgorithmName } */
+            const algorithmES = `ES${MASKS[index]}`;
             testJwtManagerSignVerify(algorithmES, EC_PRIV_KEY, EC_PUB_KEY);
         }
 
         console.log('All tests passed');
     } catch (error) {
-        console.error('Test failed:', error.message);
+        console.error('Test failed:', error);
     }
 };
 
