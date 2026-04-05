7d1c7f319316b9cb83d2ced156ca3cf3ac4c13a8
NetFeez <netfeez.dev@gmail.com>
2026-04-05 06:23:45 +0000

    refactor: expired converted in getter and separated logic in static isExpired

--- src/beta/JwtManager/Jwt.ts
+++ src/beta/JwtManager/Jwt.ts
@@ -13,10 +13,9 @@ export class Jwt implements Jwt.Jwt {
      * Checks if the JWT has expired based on the `exp` claim in the payload. If the `exp` claim is not present, it returns `false`, indicating that the token is not considered expired. If the `exp` claim is present, it compares the current time (in seconds since the Unix epoch) with the expiration time specified in the `exp` claim and returns `true` if the token has expired, or `false` otherwise.
      * @returns `true` if the JWT has expired; otherwise, `false`.
      */
-    public expired(): boolean {
+    public get expired(): boolean {
         if (!this.header.exp) return false;
-        const now = Math.floor(Date.now() / 1000);
-        return now >= this.header.exp;
+        return Jwt.isExpired(this.header.exp);
     }
     
     public toJSON(): Jwt.Jwt {
@@ -26,6 +25,16 @@ export class Jwt implements Jwt.Jwt {
         const encodedHeader = JwtUtils.objectToBase64Url(this.header);
         const encodedPayload = JwtUtils.objectToBase64Url(this.payload);
         return `${encodedHeader}.${encodedPayload}.${this.signature}`;
+    }
+
+    /**
+     * Static method to check if a given expiration time (in seconds since the Unix epoch) has passed. It compares the current time with the provided expiration time and returns `true` if the current time is greater than or equal to the expiration time, indicating that the token has expired, or `false` otherwise.
+     * @param exp The expiration time to check, represented as a number of seconds since the Unix epoch.
+     * @returns `true` if the current time is greater than or equal to the expiration time; otherwise, `false`.
+     */
+    public static isExpired(exp: number): boolean {
+        const now = Math.floor(Date.now() / 1000);
+        return now >= exp;
     }
 }
 export namespace Jwt {
