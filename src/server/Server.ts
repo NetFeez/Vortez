9ad8f647979d950e0b32a3cbc50a5db941c28064
NetFeez <netfeez.dev@gmail.com>
2026-08-10 03:55:23 +0000

    fix(Server): create router with algorithm in config

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -52,12 +52,12 @@ export class Server {
 	 */
 	public constructor(config: Server.Config | Server.Config.toProcess = {}) {
 		this.config = config instanceof Server.Config ? config : new Server.Config(config);
-		this.router = new Server.Router(this.config);
+		this.router = new Server.Router(this.config.get('routing.algorithm'));
+		this.gate = new Gate(this.config, this.router);
 		this.HttpServer = null;
 		this.HttpsServer = null;
 		this.protocol = null;
 		this.logger = _LoggerManager.getInstance();
-		this.gate = new Gate(this.router);
 		this.router.folder('/vortez:global', Path.relativeToMe(import.meta, '../../global'));
 	}
 	/**
