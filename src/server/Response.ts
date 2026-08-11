5c6c05a18186a08e17a5ab4d2290a53005fd2b11
NetFeez <netfeez.dev@gmail.com>
2026-08-11 18:16:59 +0000

    fix: file exist check before stat

--- src/server/Response.ts
+++ src/server/Response.ts
@@ -219,11 +219,11 @@ export class Response {
      */
     public async sendFile(path: string): Promise<void> {
         path = Path.normalize(path);
-        const details = await FS.promises.stat(path);
 		if  (!File.exists(path)) {
 			logger.warn(`&C2[Vortez Warning] File not found: &C6${path}`);
 			throw new ServerError(404, 'The requested URL was not found');
         }
+        const details = await FS.promises.stat(path);
         if (!details.isFile()) throw new ServerError(500, '[Response Error] - Provided path is not a file.');
         this.type(PATH.extname(path));
         if (this.vAcceptRange && this.request.headers.range) {
